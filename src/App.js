114ef139fed128506746e72e6ec72edb21fde0e7
Al3ncar <alencar29htcss@gmail.com>
2022-02-16 22:59:34 -0300

    Calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Calc extends React.Component{
+
+  state = {
+    n1:null, n2:null,
+    result: "Resultado não definido"
+  }
+  add = () => {
+    this.setState ({
+      result: Number(this.state.n1) + Number(this.state.n2)
+    })}
+  remov = () => {
+    this.setState ({
+      result: this.state.n1 - this.state.n2
+    })}
+  div = () => {
+    this.setState ({
+      result: this.state.n1 / this.state.n2
+    })}
+  mult = () => {
+    this.setState ({
+      result: this.state.n1 * this.state.n2
+    })}
+  handleChangeN1 = (e) =>{
+    this.setState({
+      n1: e.target.value
+    })}
+  handleChangeN2 = (e) =>{
+    this.setState({
+      n2: e.target.value
+    })}
+  
+  render() {
+    let {result} = this.state
+    return(
+      <main>
+        <h1>Calculadora</h1>
+        <section>
+          <div>
+          <h2>{result}</h2>
+            <input onChange={this.handleChangeN1} type="number" name="valor 1"/>
+            <input onChange={this.handleChangeN2} type="number" name="valor 2"/>
+          </div>
+          <div>
+            <button onClick={this.add}>+</button>
+            <button onClick={this.remov}>-</button>
+            <button onClick={this.div}>÷</button>
+            <button onClick={this.mult}>*</button>
+            <button onClick={this.clear}>Clear</button>
+          </div>
+        </section>
+      </main>
+    )
+  }
 }
-
-export default App;
